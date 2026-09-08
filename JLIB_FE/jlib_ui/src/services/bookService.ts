288ef6171298c1fe5a2b5d3f4fb623c29ed0a7c5
Rushikesh <rushiraj2211@gmail.com>
2026-09-08 14:13:08 +0530

    fix(api): fallback API_BASE_URL to http://127.0.0.1:8000 when env variable is missing on Vercel

--- JLIB_FE/jlib_ui/src/services/bookService.ts
+++ JLIB_FE/jlib_ui/src/services/bookService.ts
@@ -1,6 +1,6 @@
 import { IBook as Book } from '../interfaces/book-interface/ibook';
 
-const API_BASE_URL = process.env.REACT_APP_LOCAL_API_BASE_URL || '';
+const API_BASE_URL = process.env.REACT_APP_LOCAL_API_BASE_URL || 'http://127.0.0.1:8000';
 
 export async function getBooks(): Promise<Book[]> {
   try {
